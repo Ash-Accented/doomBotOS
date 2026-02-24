fef1d7a537363079046576220a2a87743a4bf577
Ash-Accented <aashirtalukder1@gmail.com>
2026-02-24 16:47:47 -0500

    Made the blueprint for the bot, made commands folder to issue commands through javascript files, made ping command, fixed little issue with ordering of the client.on callback function that was causing a lack of response from the bot

---
+++ commands/pork.js
@@ -0,0 +1,10 @@
+const { SlashCommandBuilder } = require('discord.js');
+module.exports = {
+  data: new SlashCommandBuilder().setName('pork').setDescription('Replies with ping and latency information'),
+  async execute(interaction) {
+    console.log(`hey.`);
+    const sent = await interaction.reply({ content: 'Pinging...', fetchReply: true});
+    const pingTime = sent.createdTimestamp - interaction.createdTimestamp;
+    await interaction.editReply(`EUREKA! THE BOT'S PING IS :mega: ${pingTime}ms`);
+  },
+};
